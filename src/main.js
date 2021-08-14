184f4e5491110baea4d1422138f028a1ae391530
Irene Lee <69160325+coolirene@users.noreply.github.com>
2021-08-14 02:35:14 -0400

    display items

--- src/main.js
+++ src/main.js
@@ -5,12 +5,26 @@ function loadItems() {
     .then (json => json.items);
 }
 
+// Update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item=> createHtmlString(item)).join('');
+}
+
+// Create HTML list from the given data item
+function createHtmlString(item){
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item_thumbnail">
+            <span class="item_description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
 
 // main
 loadItems() 
 .then(items => {
-    console.log(items);
-// displayItems(items);
+displayItems(items);
 // setEventListeners(items)
 
 })
